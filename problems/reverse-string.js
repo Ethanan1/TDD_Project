249b090193e5ba6592249130b400a99d08d9eed5
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-06-08 22:01:08 -0700

    reverseString solved

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,11 @@
-module.exports = function reverseString(string) {
-  // Your code here
+function reverseString(string) {
+
+  return string.split("").reverse().join("");
 };
+
+
+
+console.log(reverseString('fun'))
+
+
+module.exports = reverseString
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
 // Your code here
+const { expect } = require('chai')
+const reverseString = require('../problems/reverse-string')
+
+describe ("reverseString()", () => {
+
+    it('should reverse string', function () {
+        let word = "nuf"
+        let reverse = reverseString('fun')
+        expect(reverse).to.eql(word)
+
+        // assert.strictEqual(reverseString("fun"), "nuf")
+
+    });
+});
